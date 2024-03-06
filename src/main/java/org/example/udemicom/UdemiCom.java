4ca6d8a2e7ab8bf50089c937739cb97fae71f862
AlgosStile <oleg.todor@mail.ru>
2024-03-06 13:27:45 +0300

    added click for udemy.com

--- src/main/java/org/example/udemicom/UdemiCom.java
+++ src/main/java/org/example/udemicom/UdemiCom.java
@@ -5,6 +5,8 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
 
@@ -17,14 +19,17 @@ public class UdemiCom {
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 
         driver.get("https://www.udemy.com");
-        //! ищем элемент инпут на странице с названием name='q'
+
+        //! ищем элемент input на странице с названием name='q'
         WebElement element = driver.findElement(By.xpath("//input[@name='q']"));
-        /** вводим в инпут текст "Selenium" и нажимаем Enter
+        new WebDriverWait(driver, Duration.ofSeconds(10));
+        /** Вводим в input текст "Java" и нажимаем Enter
          * в строке ниже можно указать путь к файлу и
          * он автоматически будет загружен на сайт
          * например, element.sendKeys("..путь_к/файлу/файл.txt", Keys.ENTER);
          */
         element.sendKeys("Java", Keys.ENTER);
+
 
     }
 }
