26c4a92fd3f3dc6daa89b53326b2de91135d7c5b
AlgosStile <oleg.todor@mail.ru>
2024-05-24 14:55:02 +0300

    added allure+

--- src/main/java/org/example/testNG/tests/Test2.java
+++ src/main/java/org/example/testNG/tests/Test2.java
@@ -1,10 +1,11 @@
-package org.example.testNG.tests;
-
-import org.testng.annotations.Test;
-
-public class Test2 {
-    @Test(groups = {"smokeTest", "regress"})
-    public void  test2 () {
-        System.out.println("Test 2");
-    }
-}
+//package org.example.testNG.tests;
+//
+//
+//import org.junit.Test;
+//
+//public class Test2 {
+//    @Test(groups = {"smokeTest", "regress"})
+//    public void  test2 () {
+//        System.out.println("Test 2");
+//    }
+//}
